82825414a806cb4cc5649232ed7c64e6402ab1ba
ydo30 <danieldo2214@gmail.com>
2016-02-07 18:44:41 -0500

    Added a toast when logout

--- app/src/main/java/edu/gatech/teamelevenproject/MovieSelector.java
+++ app/src/main/java/edu/gatech/teamelevenproject/MovieSelector.java
@@ -1,11 +1,13 @@
 package edu.gatech.teamelevenproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MovieSelector extends AppCompatActivity {
 
@@ -28,6 +30,11 @@ public class MovieSelector extends AppCompatActivity {
             Intent back = new Intent(getBaseContext(), MainActivity.class);
             back.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
             startActivity(back);
+            String text = "Logout Success!";
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast t = Toast.makeText(context, text, duration);
+            t.show();
         }
 
         return super.onOptionsItemSelected(item);
