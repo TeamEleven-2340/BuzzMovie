c9508266ad4efcf4beb38059a008e5ff3f510755
julietkb <julietkbenjamin@gmail.com>
2016-04-03 00:44:43 -0400

    Checked DatabaseWrapper

--- app/src/main/java/edu/gaTech/teamElevenProject/AuthenticationFacade.java
+++ app/src/main/java/edu/gaTech/teamElevenProject/AuthenticationFacade.java
@@ -4,6 +4,7 @@ package edu.gaTech.teamElevenProject;
  * Created by Admin on 2/2/2016.
  */
 public interface AuthenticationFacade {
+
     /**
      * Handle login request
      * @param name name of the user
--- app/src/main/java/edu/gaTech/teamElevenProject/DatabaseWrapper.java
+++ app/src/main/java/edu/gaTech/teamElevenProject/DatabaseWrapper.java
@@ -8,128 +8,159 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Admin on 3/21/2016.
  */
 public class DatabaseWrapper extends SQLiteOpenHelper {
+
     /**
      * User database column name
      */
     public static final String USER = "Users";
+
     /**
      * User database column name
      */
     public static final String USERNAME = "Username";
+
     /**
      * User database column name
      */
     public static final String PASSWORD = "Password";
+
     /**
      * User database column name
      */
     public static final String MAJOR = "Major";
+
     /**
      * User database column name
      */
     public static final String banStatus = "banStatus";
+
     /**
      * User database column name
      */
     public static final String lockStatus = "lockStatus";
+
     /**
      * User database column name
      */
     public static final String fullName = "fullName";
+
     /**
      * User database column name
      */
     public static final String INTEREST = "Interest";
+
     /**
      * User database column name
      */
     public static final String EMAIL = "Email";
+
     /**
      * User database column name
      */
     public static final String adminStatus = "AdminStatus";
+
     /**
      * User database column name
      */
     public static final String DATABASE_NAME = "Users.db";
+
     /**
      * User database name
      */
     public static final String DATABASE_MOVIE_NAME = "Movie.db";
+
     /**
      * User database version
      */
     private static final int DATABASE_VERSION = 1;
+
     /**
      * Movie database column name
      */
     public static final String MOVIE = "Movie";
+
     /**
      * Movie database column name
      */
     public static final String MOVIE_NAME = "Name";
+
     /**
      * Movie database column name
      */
     public static final String RATING = "Rating";
+
     /**
      * Movie database column name
      */
     public static final String csRating = "CSRating";
+
     /**
      * Movie database column name
      */
     public static final String meRating = "MERating";
+
     /**
      * Movie database column name
      */
     public static final String ceRating = "CERating";
+
     /**
      * Movie database column name
      */
     public static final String eeRating = "EERating";
+
     /**
      * Movie database column name
      */
     public static final String ratedPeople = "RatedPeople";
+
     /**
      * Movie database column name
      */
     public static final String csPeopleRated = "CSRatedPeople";
+
     /**
      * Movie database column name
      */
+
     /**
      * Movie database column name
      */
     public static final String mePeopleRated = "MERatedPeople";
+
     /**
      * Movie database column name
      */
     public static final String cePeopleRated = "CERatedPeople";
+
     /**
      * Movie database column name
      */
     public static final String eePeopleRated = "EERatedPeople";
+
     /**
      * String name
      */
     private static final String TNN = " text not null, ";
+
     /**
      * Database creation for Users.db
      */
-    private static final String DATABASE_CREATE = "CREATE TABLE " + USER
-            + " (" + USERNAME + TNN + PASSWORD + TNN + EMAIL + TNN
-            + fullName + TNN + MAJOR + TNN + INTEREST
-            + TNN + lockStatus + TNN + banStatus + TNN
-            + adminStatus + " text not null)";
+    private static final String DATABASE_CREATE = "CREATE TABLE "
+            + USER + " (" + USERNAME + TNN + PASSWORD + TNN
+            + EMAIL + TNN + fullName + TNN + MAJOR + TNN
+            + INTEREST + TNN + lockStatus + TNN + banStatus
+            + TNN + adminStatus + " text not null)";
+
     /**
      * Database creation for Movie.db
      */
-    private static final String MOVIE_DATABASE_CREATE = "CREATE TABLE " + MOVIE + " ("+MOVIE_NAME +TNN + RATING + TNN
-            + csRating + TNN + meRating + TNN + ceRating + TNN +
-            eeRating + TNN + ratedPeople + TNN + csPeopleRated + TNN + mePeopleRated
-            + TNN + cePeopleRated + TNN + eePeopleRated + " text not null)";
+    private static final String MOVIE_DATABASE_CREATE
+            = "CREATE TABLE " + MOVIE + " (" + MOVIE_NAME
+            + TNN + RATING + TNN + csRating + TNN + meRating
+            + TNN + ceRating + TNN + eeRating + TNN + ratedPeople
+            + TNN + csPeopleRated + TNN + mePeopleRated + TNN
+            + cePeopleRated + TNN + eePeopleRated + " text not null)";
 
     /**
      * Constructor
